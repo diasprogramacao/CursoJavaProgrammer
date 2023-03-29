ebd610cc9150aad8e11f29ff5bfe0086b54d7fd2
Alan Dias da Silva <diasprogramacao@gmail.com>
2023-03-28 22:57:27 -0300

    Criado 3 variáveis e importado a classe Scanner para interagir com as informações digitadas no console

--- Aula-28-03-2023/MeuPrimeiroApp/Principal.java
+++ Aula-28-03-2023/MeuPrimeiroApp/Principal.java
@@ -1,8 +1,25 @@
+import java.util.Scanner;
+
 public class Principal {
 
 public static void main (String [] agrs) {
 
-    System.out.println("Meu Primeiro Commmit ");
+	Scanner entradaDados = new Scanner(System.in); 
+
+	System.out.println("Digite o seu nome");
+      String nome = entradaDados.next();
+
+	System.out.println("Digite o seu idade");
+      String idade = entradaDados.next();
+
+      System.out.println("Digite o seu endereco");
+      String endereco = entradaDados.next();
+		
+	System.out.println("Nome digitado : " + nome);
+      System.out.println("Idade digitado : " + idade);
+      System.out.println("Endereco digitado : " + endereco);
+
+
 
      }
 
