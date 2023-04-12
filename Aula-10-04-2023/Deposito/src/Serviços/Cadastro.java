4a934b2bee370db3bbc007a6608575f73d125e7b
Alan Dias da Silva <diasprogramacao@gmail.com>
2023-04-11 21:12:50 -0300

    complemento

--- Aula-10-04-2023/Deposito/src/Serviços/Cadastro.java
+++ Aula-10-04-2023/Deposito/src/Serviços/Cadastro.java
@@ -9,7 +9,11 @@ import entidades.Makita;
 
 public class Cadastro {
 	
-	public void cadastrarFuradeira(Furadeira objeto) {
+	
+	
+	public void cadastrarFuradeira() {
+		
+		Furadeira objeto= new Furadeira();
 		
 		JOptionPane.showMessageDialog(null, "CADASTRO DA FURADEIRA");
 		objeto.setCodigo(Integer.parseInt(JOptionPane.showInputDialog("Digite o código do equipamento: ")));
@@ -17,10 +21,14 @@ public class Cadastro {
 		objeto.setPrecoDeCompra(Double.parseDouble(JOptionPane.showInputDialog("Digite o preço de compra do equipamento: ")));
 		objeto.setPrecoDeVenda(Double.parseDouble(JOptionPane.showInputDialog("Digite o valor de venda do equipamento: ")));
 		objeto.setModeloFuradeira(JOptionPane.showInputDialog("Digite a modelo do equipamento: "));
-
+		objeto.setLucro(objeto.getPrecoDeVenda()- objeto.getPrecoDeCompra());
+		
+		exibirFuradeira(objeto);
+		
 	}
 
-	public void exibirFuradeira(Furadeira objeto,CalculoLucro objetoLucro) {
+	public void exibirFuradeira(Furadeira objeto) {
+		
 		
 		JOptionPane.showMessageDialog(null, "DADOS DO EQUIPAMENTO FURADEIRA");
 		JOptionPane.showMessageDialog(null, "Código: " + objeto.getCodigo());
@@ -28,11 +36,13 @@ public class Cadastro {
 		JOptionPane.showMessageDialog(null, "Preço de compra: " + objeto.getPrecoDeCompra());
 		JOptionPane.showMessageDialog(null, "Preço de venda: " + objeto.getPrecoDeVenda());
 		JOptionPane.showMessageDialog(null, "Modelo do Equipamento : " + objeto.getModeloFuradeira());
-		JOptionPane.showMessageDialog(null, "Lucro : " + objetoLucro.lucro(objeto));
+		JOptionPane.showMessageDialog(null, "Lucro : " + objeto.getLucro());
 	}
 	
 	
-public void cadastrarMakita(Makita objeto) {
+public void cadastrarMakita() {
+	
+		Makita objeto = new Makita();
 		
 	    JOptionPane.showMessageDialog(null, "CADASTRO DO EQUIPAMENTO MAKITA");
 		objeto.setCodigo(Integer.parseInt(JOptionPane.showInputDialog("Digite o código do equipamento: ")));
@@ -40,10 +50,13 @@ public void cadastrarMakita(Makita objeto) {
 		objeto.setPrecoDeCompra(Double.parseDouble(JOptionPane.showInputDialog("Digite o preço de compra do equipamento: ")));
 		objeto.setPrecoDeVenda(Double.parseDouble(JOptionPane.showInputDialog("Digite o valor de venda do equipamento: ")));
 		objeto.setModeloMakita(JOptionPane.showInputDialog("Digite a modelo do equipamento: "));
-
+		objeto.setLucro(objeto.getPrecoDeVenda() - objeto.getPrecoDeCompra());
+		
+		exibirMakita(objeto);
+		
 	}
 
-public void exibirMakita(Makita objeto,CalculoLucro objetoLucro) {
+public void exibirMakita(Makita objeto) {
 	
 	JOptionPane.showMessageDialog(null, "DADOS DO EQUIPAMENTO MAKITA");
 	JOptionPane.showMessageDialog(null, "Código: " + objeto.getCodigo());
@@ -51,9 +64,24 @@ public void exibirMakita(Makita objeto,CalculoLucro objetoLucro) {
 	JOptionPane.showMessageDialog(null, "Preço de compra: " + objeto.getPrecoDeCompra());
 	JOptionPane.showMessageDialog(null, "Preço de venda: " + objeto.getPrecoDeVenda());
 	JOptionPane.showMessageDialog(null, "Modelo do Equipamento : " + objeto.getModeloMakita());
-	JOptionPane.showMessageDialog(null, "Lucro : " + objetoLucro.lucro(objeto));
+	JOptionPane.showMessageDialog(null, "Lucro : " + objeto.getLucro());
 }
 	
+
+  public void menu () {
+	  
+	   int id = 0;
+			  
+	  id = Integer.parseInt(JOptionPane.showInputDialog(" Cadastro de Furadeira digite 1 e Cadastro de Makita digite 2"));
+	  
+	  Cadastro cadastro = new Cadastro();
+	 
+	 if (id == 1) {
+		 cadastro.cadastrarFuradeira();
+	 } else {
+		 cadastro.cadastrarMakita();
+	 }
+  }
 	
 	
 }
